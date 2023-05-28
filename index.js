a3a8912aaa380877be8488302274306a046e3b80
thatkhay <johnkingsleyegeonu@gmail.com>
2023-05-28 19:48:48 +0100

    lets connet js logic

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const menu = document.querySelector('.menuDiv');
 const list = document.querySelectorAll('li');
 let isMenuVisible = false;
+const socialsButton = document.querySelector('.socialsButton');
 
 menu.addEventListener('click', toggleMenu);
 
 function toggleMenu() {
-    menu.classList.toggle('active');
+  menu.classList.toggle('active');
   isMenuVisible = !isMenuVisible; // Toggle the visibility flag
 
   if (isMenuVisible) {
@@ -13,4 +14,11 @@ function toggleMenu() {
   } else {
     list.forEach(li => li.style.display = 'none');
   }
+}
+
+socialsButton.addEventListener('click', socialsDisplay);
+
+function socialsDisplay() {
+  const apps = document.querySelectorAll('.apps');
+  apps.forEach(img => img.style.display = 'flex');
 }
